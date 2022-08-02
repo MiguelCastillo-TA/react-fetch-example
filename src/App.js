785b1489bdd96c8c8840b20e9f07bfff9b10da46
Miguel Castillo <castilma95@gmail.com>
2022-08-01 18:07:05 -0600

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [allPokemon, setAllPokemons] = useState([])
+  const [currentPokemon, setCurrentPokemon] = useState({})
+
+  useEffect(() => {
+    console.log('useffect')
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then(response => {
+        return response.json();
+      }).then(response => {
+          console.log(response.results);
+          setAllPokemons(response.results);
+      }).catch(err=>{
+          console.log(err);
+      });
+  }, [])
+
+  const callPokemonDetails = (url) => {
+    console.log(url)
+    fetch(url)
+    .then(response => {
+      return response.json();
+    }).then(response => {
+        console.log(response);
+        setCurrentPokemon(response)
+    }).catch(err=>{
+        console.log(err);
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>HERE ARE ALL 151 POKEMON</h1>
+
+      {Object.keys(currentPokemon).length > 0 ? 
+        <div>
+          <p>{currentPokemon.name}</p>
+          <p>{currentPokemon.height}</p>
+          <p>{currentPokemon.weight}</p>
+        </div>
+      : <p>no current pokemone</p>}
+      {
+        allPokemon.length > 0 ? allPokemon.map((pokemon) => {
+          return(
+            <div key={`${pokemon.name}`}>
+              <p>{pokemon.name}</p>
+              <button onClick={() => callPokemonDetails(pokemon.url)}>view more details</button>
+            </div>
+          )
+        }) : <p>loading</p>
+      }
     </div>
   );
 }
